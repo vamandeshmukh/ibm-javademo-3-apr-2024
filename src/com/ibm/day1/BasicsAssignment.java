5ddb8018c615c3c304b736306aaf9a58d705264b
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-04-03 17:22:42 +0530

    java-day1

--- src/com/ibm/day1/BasicsAssignment.java
+++ src/com/ibm/day1/BasicsAssignment.java
@@ -1,18 +1,36 @@
 package com.ibm.day1;
 
 public class BasicsAssignment {
-	
+
+	public static void findBerthType(int berthNumber) {
+
+		String berthType = ""; // lower, middle, upper, side-lower, side-upper
+
+		// your code
+		if (berthNumber >= 1 && berthNumber <= 72) {
+			if (berthNumber % 8 == 1 || berthNumber % 8 == 4)
+				berthType = "lower";
+			else if (berthNumber % 8 == 2 || berthNumber % 8 == 5)
+				berthType = "middle";
+			else if (berthNumber % 8 == 3 || berthNumber % 8 == 6)
+				berthType = "upper";
+			else if (berthNumber % 8 == 7)
+				berthType = "side-lower";
+			else
+				berthType = "side-upper";
+		} else {
+			berthType = "'Invalid'";
+		}
+
+		System.out.println("Your berth number is " + berthNumber + " which is " + berthType + ".");
+
+	}
+
 	public static void main(String[] args) {
-		
-		int berthNumber = 1; // 1 to 72 
-		
-		String berthType = "";  // lower, middle, upper, side-lower, side-upper 
-		
-		// your code 
-		
-		System.out.println("Your berth number is " + berthNumber + " and your berth type is " + berthType + ".");
-		
-		
+		for (int i = 0; i <= 73; i++) {
+			BasicsAssignment.findBerthType(i);
+		}
+
 	}
 
 }
