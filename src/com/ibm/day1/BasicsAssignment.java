9a6a1573d6b87e18444b5dfafd718107b243db5c
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-04-03 16:54:13 +0530

    java-day1

--- src/com/ibm/day1/BasicsAssignment.java
+++ src/com/ibm/day1/BasicsAssignment.java
@@ -4,6 +4,14 @@ public class BasicsAssignment {
 	
 	public static void main(String[] args) {
 		
+		int berthNumber = 1;
+		
+		String berthType = "";
+		
+		// your code 
+		
+		System.out.println("Your berth number is " + berthNumber + " and your berth type is " + berthType + ".");
+		
 		
 	}
 
