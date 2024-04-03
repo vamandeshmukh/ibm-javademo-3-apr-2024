7f581fef5b756ef49f92909c2e05ea36d568abf3
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-04-03 15:26:13 +0530

    java-day1

--- src/com/ibm/day1/OperatorsDemo.java
+++ src/com/ibm/day1/OperatorsDemo.java
@@ -4,13 +4,35 @@ public class OperatorsDemo {
 	
 	public static void main(String[] args) {
 		
-		int num = 10;
+//		int num = 10;
 		
-		System.out.println(num); // 10
-		System.out.println(num++); // 10 
-		System.out.println(++num); // 12
-		System.out.println(num--); // 12
-		System.out.println(--num); // 10
+//		System.out.println(num); // 10
+//		System.out.println(num++); // 10 
+//		System.out.println(++num); // 12
+//		System.out.println(num--); // 12
+//		System.out.println(--num); // 10
+		
+//		+ - * / %
+		
+//		System.out.println(10 / 4); // 2
+//		System.out.println(10 / 3); // 3
+//		System.out.println(10 % 3); // 1
+//		System.out.println(10 % 5); // 0
+		
+//		int num1 = 10, num2 = 20;
+//		
+//		System.out.println( num1 > num2); // F
+//		System.out.println( num1 >= num2); // F
+//		System.out.println( num1 < num2); // T
+//		System.out.println( num1 <= num2); // T
+//		System.out.println( num1 == num2); // F
+//		System.out.println( num1 != num2); // T
+		
+		boolean isMarried = false;
+		
+		System.out.println(isMarried); // false
+		System.out.println(!isMarried); // true
+		
 		
 	}
 
