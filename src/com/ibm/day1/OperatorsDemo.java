ec06881c2029d4d7f58820799163bddb0e0f3eee
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-04-03 15:18:41 +0530

    java-day1

---
+++ src/com/ibm/day1/OperatorsDemo.java
@@ -0,0 +1,17 @@
+package com.ibm.day1;
+
+public class OperatorsDemo {
+	
+	public static void main(String[] args) {
+		
+		int num = 10;
+		
+		System.out.println(num); // 10
+		System.out.println(num++); // 10 
+		System.out.println(++num); // 12
+		System.out.println(num--); // 12
+		System.out.println(--num); // 10
+		
+	}
+
+}
