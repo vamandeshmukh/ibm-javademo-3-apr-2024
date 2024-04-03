37689d8f077b0a38063aa959d5bf85a24d792ca3
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-04-03 17:28:25 +0530

    java-day1

--- src/com/ibm/day1/BasicsAssignment.java
+++ src/com/ibm/day1/BasicsAssignment.java
@@ -3,23 +3,27 @@ package com.ibm.day1;
 public class BasicsAssignment {
 
 	public static String findBerthType(int berthNumber) {
+		
+//		berthNumber 1-72
+
 		String response = "";
-		String berthType = ""; // lower, middle, upper, side-lower, side-upper
+		String typeOfBerth = ""; // lower, middle, upper, side-lower, side-upper
+		
 		if (berthNumber >= 1 && berthNumber <= 72) {
 			if (berthNumber % 8 == 1 || berthNumber % 8 == 4)
-				berthType = "lower";
+				typeOfBerth = "lower";
 			else if (berthNumber % 8 == 2 || berthNumber % 8 == 5)
-				berthType = "middle";
+				typeOfBerth = "middle";
 			else if (berthNumber % 8 == 3 || berthNumber % 8 == 6)
-				berthType = "upper";
+				typeOfBerth = "upper";
 			else if (berthNumber % 8 == 7)
-				berthType = "side-lower";
+				typeOfBerth = "side-lower";
 			else
-				berthType = "side-upper";
+				typeOfBerth = "side-upper";
 		} else {
-			berthType = "'Invalid'";
+			typeOfBerth = "'Invalid'";
 		}
-		response = "Your berth number is " + berthNumber + " which is " + berthType + ".";
+		response = "Your berth number is " + berthNumber + " which is " + typeOfBerth + ".";
 		return response;
 	}
 
