5548a4bd663acaab4a50fce544ca08fb77196fe0
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-04-04 10:03:14 +0530

    java day2

--- src/com/ibm/day2/oop/ClassDemo.java
+++ src/com/ibm/day2/oop/ClassDemo.java
@@ -8,13 +8,30 @@ public class ClassDemo {
 		System.out.println(billAmount);
 	}
 
+	static double gstBillCalc(double amount) {
+		double gst = 18;
+		double billAmount = amount + (amount * (gst / 100));
+		return billAmount;
+	}
+
 	public static void main(String[] args) {
-		
+
+//		gstBillPrint(350);
+//		gstBillPrint(500);
+//		gstBillPrint(950);
+
 		ClassDemo.gstBillPrint(350);
 		ClassDemo.gstBillPrint(500);
 		ClassDemo.gstBillPrint(950);
 
+		double finalBillAmount = ClassDemo.gstPctCalc(200, 6);
+		System.out.println(finalBillAmount); // 212
 
+		double finalBillAmount2 = ClassDemo.gstPctCalc(200, 18);
+		System.out.println(finalBillAmount2); // 236 
+
+		double finalBillAmount3 = ClassDemo.gstPctCalc(200, 12);
+		System.out.println(finalBillAmount3); // 224
 	}
 
 }
