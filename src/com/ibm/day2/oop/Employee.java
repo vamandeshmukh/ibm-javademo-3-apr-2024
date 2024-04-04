965c8e5c2a118497a2cddd6268d7f418f894793c
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-04-04 10:34:19 +0530

    java day2

--- src/com/ibm/day2/oop/Employee.java
+++ src/com/ibm/day2/oop/Employee.java
@@ -6,6 +6,24 @@ public class Employee {
 	String firstName;
 	double salary;
 
+	public Employee() {
+		System.out.println("default constructor");
+	}
+
+// 		public Employee(int eid, String name, double sal) {
+//		System.out.println("parameterized constructor");
+//		this.employeeId = eid;
+//		this.firstName = name;
+//		this.salary = sal;
+//	}
+
+	public Employee(int employeeId, String firstName, double salary) {
+		super();
+		this.employeeId = employeeId;
+		this.firstName = firstName;
+		this.salary = salary;
+	}
+
 	String getEmployeeData() {
 		return "Employee id: " + employeeId + " Name: " + firstName + " salary: " + salary;
 	}
