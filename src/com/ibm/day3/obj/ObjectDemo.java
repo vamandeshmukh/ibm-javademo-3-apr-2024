01a79c030eb2a6abff652e48143538b44898ca21
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-04-05 10:42:42 +0530

    java day-3

--- src/com/ibm/day3/obj/ObjectDemo.java
+++ src/com/ibm/day3/obj/ObjectDemo.java
@@ -6,7 +6,12 @@ public class ObjectDemo {
 
 		Employee emp = new Employee(101, "Sonu", 10.5);
 
-		System.out.println(emp.getFirstName());
+//		System.out.println(emp.getEmployeeId());
+//		System.out.println(emp.getFirstName());
+//		System.out.println(emp.getSalary());
+//		System.out.println(emp.getFirstName() + " " + emp.getSalary() + " " + emp.getEmployeeId());
+		
+		System.out.println(emp.toString());
 
 	}
 
