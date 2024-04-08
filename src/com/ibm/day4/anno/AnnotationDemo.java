bbfdb3a2aab8b7bf421000faddaec369e35e3613
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-04-08 12:37:14 +0530

    day4 java

---
+++ src/com/ibm/day4/anno/AnnotationDemo.java
@@ -0,0 +1,36 @@
+package com.ibm.day4.anno;
+
+@interface MyAnon {
+	
+	String val();
+
+}
+
+@MyAnon(val = "abc")
+class Class1 {
+
+}
+
+@FunctionalInterface
+interface Int {
+
+	void m1();
+
+}
+
+class C1 implements Int {
+
+	@Override
+	public void m1() {
+
+	}
+
+}
+
+public class AnnotationDemo {
+
+	public static void main(String[] args) {
+r
+	}
+
+}
---
+++ src/com/ibm/day4/gc/GarbageDemo.java
@@ -0,0 +1,14 @@
+package com.ibm.day4.gc;
+
+public class GarbageDemo {
+	
+	public static void main(String[] args) {
+		
+		String str = "abc";
+		str = null;
+		
+		
+		
+	}
+
+}
